c44a950daaf8a892d8009f27c1123db1411256fa
Daniel Reina <dannyflatiron@gmail.com>
2020-01-10 02:20:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,11 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    binding.pry
+    students_array.each do |student|
+      Student.new(student)
+    end
+
+    
   end
 
   def add_student_attributes(attributes_hash)
