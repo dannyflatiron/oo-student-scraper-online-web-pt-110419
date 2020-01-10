2ebc64b6e88256e4d5deb7cc4447821b99d3edb4
Daniel Reina <dannyflatiron@gmail.com>
2020-01-10 01:34:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,7 @@ class Student
     #takes in an argument of a hash and sets that new student's attributes using the key/value pairs of that hash. (FAILED -
     # how do I set key value pair of a hash
     student_hash.each do |key, value|
-      self.name = value[0]
+      self.name = student_hash.values[0]
       # binding.pry
     end
     # binding.pry
